5a8d6c3e31f809da767e3ad59d20cf9c46effade
Asta <asta.dev.520@gmail.com>
2024-04-09 23:10:03 +0700

    fix: remove redux provider to render page as SSR

--- src/components/home/HomePage.tsx
+++ src/components/home/HomePage.tsx
@@ -1,16 +1,10 @@
 "use client";
-import { getAllCategories, getProducts } from "@/api";
+
 import { Category, Product } from "@/shared/interfaces";
-import { useQuery } from "@tanstack/react-query";
 import Image from "next/image";
 import { Swiper, SwiperSlide } from "swiper/react";
 // Import Swiper styles
-import { useAppSelector } from "@/hooks/redux";
-import useDebounce from "@/hooks/useDebounce";
-import usePagination from "@/hooks/usePagination";
 import Link from "next/link";
-import React from "react";
-import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 import "swiper/css";
 import "swiper/css/navigation";
@@ -25,35 +19,6 @@ export default function Home({
   categories: Category[];
   products: Product[];
 }) {
-  // const { search } = useAppSelector((state) => state.search);
-  // const { nextPage, prevPage, handlePageButtonClick, page } = usePagination(2);
-  // const debouncedSearch = useDebounce(search, 500);
-
-  const { data: getCategories, isFetching: isFetchingCategories } = useQuery({
-    queryKey: ["categories"],
-    queryFn: () => getAllCategories(),
-    initialData: {
-      categories,
-    },
-  });
-
-  // const {
-  //   data: _products,
-  //   isFetching: isFetchingProducts,
-  //   refetch,
-  // } = useQuery({
-  //   queryKey: ["products", page, debouncedSearch],
-  //   queryFn: () => getProducts({ page, search: debouncedSearch }),
-  //   initialData: {
-  //     products,
-  //   },
-  // });
-
-  // React.useEffect(() => {
-  //   refetch();
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, [debouncedSearch]);
-
   return (
     <>
       <div className="max-w-5xl mx-auto bg-white">
@@ -110,45 +75,30 @@ export default function Home({
         <div className="shadow p-3 my-5">
           <p className="font-bold text-lg">Categories</p>
           <div className="grid grid-cols-3 lg:grid-cols-7 items-start mt-5 text-center">
-            {false
-              ? Array(8)
-                  .fill(null)
-                  .map((_, index) => (
-                    <div key={index} className={`rounded-b-lg`}>
-                      <a href="#" className="flex flex-col items-center">
-                        <div className="relative h-20 w-20">
-                          <Skeleton className="h-full rounded-b-lg" />
-                        </div>
-                        <p className="text-sm w-1/2 line-clamp-2 text-gray-600 mt-2">
-                          <Skeleton />
-                        </p>
-                      </a>
-                    </div>
-                  ))
-              : categories?.map((category: Category) => {
-                  return (
-                    <Link
-                      href={`/category/${category.id}`}
-                      key={category.id}
-                      className={`flex flex-col items-center rounded-b-lg ${
-                        category.status.trim() === "Published" ? "" : "hidden"
-                      }`}
-                    >
-                      <div className="relative h-20 w-20">
-                        <Image
-                          src={category?.image ?? "/images/category.jpeg"}
-                          className="object-cover rounded-3xl"
-                          alt={category.name}
-                          loading="lazy"
-                          fill={true}
-                        />
-                      </div>
-                      <p className="text-sm line-clamp-2 text-gray-600 mt-2">
-                        {category.name}
-                      </p>
-                    </Link>
-                  );
-                })}
+            {categories?.map((category: Category) => {
+              return (
+                <Link
+                  href={`/category/${category.id}`}
+                  key={category.id}
+                  className={`flex flex-col items-center rounded-b-lg ${
+                    category.status.trim() === "Published" ? "" : "hidden"
+                  }`}
+                >
+                  <div className="relative h-20 w-20">
+                    <Image
+                      src={category?.image ?? "/images/category.jpeg"}
+                      className="object-cover rounded-3xl"
+                      alt={category.name}
+                      loading="lazy"
+                      fill={true}
+                    />
+                  </div>
+                  <p className="text-sm line-clamp-2 text-gray-600 mt-2">
+                    {category.name}
+                  </p>
+                </Link>
+              );
+            })}
           </div>
         </div>
         <div className="shadow p-3 my-5">
--- src/components/navbar/SearchBar.tsx
+++ src/components/navbar/SearchBar.tsx
@@ -1,15 +1,13 @@
-import { setSearch } from "@/features";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import type { DispatchProp } from "react-redux";
 
-export function SearchBar({ dispatch }: DispatchProp) {
+export function SearchBar() {
   return (
     <li className="max-w-2xl flex-grow mx-auto">
       <div className="relative">
         <input
           type="search"
-          onChange={(e) => dispatch(setSearch(e.target.value))}
+          onChange={() => console.log("set search on url")}
           id="default-search"
           className="block p-2.5 w-full text-sm text-gray-900 bg-white rounded border-none border-gray-300 focus:ring-0 ring-0"
           placeholder="Search your product ..."
