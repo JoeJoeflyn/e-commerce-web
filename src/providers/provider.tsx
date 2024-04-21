1029f3881fbfb152ffabc5bd48f7508f803ca3d3
JoeJoeflyn <thaitainguyen336@gmail.com>
2024-04-21 18:51:32 +0700

    fix: fixing posthog config the 3rd times

--- src/providers/provider.tsx
+++ src/providers/provider.tsx
@@ -6,9 +6,11 @@ import posthog from "posthog-js";
 import { PostHogProvider } from "posthog-js/react";
 
 if (typeof window !== "undefined") {
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-  });
+  if (!posthog.__loaded) {
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+      api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    });
+  }
 }
 
 const queryClient = new QueryClient();
