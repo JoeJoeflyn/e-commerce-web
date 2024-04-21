c6f4618d88d729e292128b77f7a0b5b95e66ad1c
JoeJoeflyn <thaitainguyen336@gmail.com>
2024-04-21 16:57:44 +0700

    feat: adding posthog for tracking user event

--- src/providers/provider.tsx
+++ src/providers/provider.tsx
@@ -2,13 +2,24 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
+
+if (typeof window !== "undefined") {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST!,
+  });
+}
+
 const queryClient = new QueryClient();
 
 export default function Providers({ children }: any) {
   return (
-    <QueryClientProvider client={queryClient}>
-      {children}
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <PostHogProvider client={posthog}>
+      <QueryClientProvider client={queryClient}>
+        {children}
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </PostHogProvider>
   );
 }
